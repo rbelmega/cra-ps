27bf902515441a6466de55859cac7f7522eae6e7
rbelmeha <belmega31@gmail.com>
2018-04-07 22:26:04 +0300

    process.env.PORT

--- src/server/index.js
+++ src/server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const router = express.Router();
 // root (/) should always serve our server rendered page
 // other static resources should just be served as they are
-router.use('*.*',
+router.use(
   express.static(path.resolve(__dirname, '../..', 'build/'), { maxAge: '30d' })
 );
 router.use('*', serverRenderer);
