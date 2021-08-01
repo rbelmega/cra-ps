a33eee60687fd5880f115419d01c2e10c32778e0
Rostyslav <belmega31@gmail.com>
2021-08-01 16:03:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const port = 3000;
 const alive = new StartKeepAlive();
 alive.run();
 
+app.get('/.well-known/acme-challenge/sPOLZJlWdxXKmzA-BP6r6FjIQYN-iTA8vm8NQbA4mVE', function(req, res) {
+  res.send('sPOLZJlWdxXKmzA-BP6r6FjIQYN-iTA8vm8NQbA4mVE.EvRjYLH3luPn69sTwa56hSZYDSkKrCx1mLUfIwq-WbU')
+})
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
