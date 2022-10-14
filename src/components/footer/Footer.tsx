f987bfb289c351c27938302eacb3270e631c6370
Rostyslav Belmeha <belmega31@gmail.com>
2022-10-14 23:29:23 +0300

    feat: next.js instagram api

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { BlogList } from '../blog-list';
-import styles from './Footer.module.scss';
+import React, { useEffect, useState } from "react";
+import { BlogList } from "../blog-list";
+import styles from "./Footer.module.scss";
 
 export const Footer = () => {
   const [instagramData, setInstagramData] = useState([]);
 
   useEffect(() => {
-    fetch('/api/instagram')
+    fetch("/api/instagram")
       .then((response) => response.json())
       .then(setInstagramData);
   }, []);
@@ -15,13 +15,13 @@ export const Footer = () => {
     <div>
       <div className={styles.instagramWrapper}>
         {instagramData.map((media) =>
-          media.media_type === 'IMAGE' ? (
+          media.media_type === "IMAGE" ? (
             <a key={media.media_url} href={media.media_url} target="_blank">
               <img src={media.media_url} />
             </a>
           ) : (
             <a key={media.media_url} href={media.media_url} target="_blank">
-              <video autoPlay={true} src={media.media_url}></video>
+              <video autoPlay={true} muted={true} src={media.media_url}></video>
             </a>
           )
         )}
