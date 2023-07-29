e81db624a79021b6ba29e8ad7de17596e052c81a
Rostyslav Belmeha <belmega31@gmail.com>
2023-07-29 19:00:51 +0300

    GA 4

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /* config options here */
     env: {
-        PUBLIC_GOOGLE_ANALYTICS_V4: 'G-V05EW6SZRE'
+        PUBLIC_GOOGLE_ANALYTICS_V4: process.env.PUBLIC_GOOGLE_ANALYTICS_V4
     }
 }
 
