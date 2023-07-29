f95ed045d439aa04082782e199139e4f907c4d9e
Rostyslav Belmeha <belmega31@gmail.com>
2023-07-29 14:49:51 +0300

    GA 4

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-if (process.env.NODE_ENV === 'development') {
-    require('dotenv').config()
-}
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /* config options here */
     env: {
-        PUBLIC_GOOGLE_ANALYTICS_V4: process.env.PUBLIC_GOOGLE_ANALYTICS_V4
+        PUBLIC_GOOGLE_ANALYTICS_V4: 'G-V05EW6SZRE'
     }
 }
 
