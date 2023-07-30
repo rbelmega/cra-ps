645ac092ded4146b8c9edc7479245fee108b6e2f
Rostyslav Belmeha <belmega31@gmail.com>
2023-07-30 17:48:06 +0300

    alt

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -17,7 +17,7 @@ export const Footer = () => {
         {instagramData.map((media) =>
           media.media_type === 'IMAGE' ? (
             <a key={media.media_url} href={media.media_url} target="_blank">
-              <img src={media.media_url} />
+              <img src={media.media_url} alt={media.caption}/>
             </a>
           ) : (
             <a key={media.media_url} href={media.media_url} target="_blank">
