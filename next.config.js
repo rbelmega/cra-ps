049435d097fa3c26fc10c5fb4e3ee6f86f14f7e5
Rostyslav Belmeha <belmega31@gmail.com>
2023-10-15 16:27:12 +0300

    feat: versions update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'scontent-iad3-2.cdninstagram.com',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { BlogList } from '../blog-list';
 import { getPosts } from '../../domain';
 import styles from './Footer.module.scss';
@@ -12,7 +13,12 @@ export const Footer = async () => {
         {instagramData.map((media) =>
           media.media_type === 'IMAGE' ? (
             <a key={media.media_url} href={media.media_url} target="_blank">
-              <img src={media.media_url} alt={media.caption || ''} />
+              <Image
+                width={140}
+                height={140}
+                src={media.media_url}
+                alt={media.caption || ''}
+              />
             </a>
           ) : (
             <a key={media.media_url} href={media.media_url} target="_blank">
