3e69a10fc460f628dfae041cdd5c9c961c30b83e
Rostyslav Belmeha <belmega31@gmail.com>
2023-10-20 22:33:48 +0300

    feat: versions update

--- src/domain/instagram.ts
+++ src/domain/instagram.ts
@@ -2,7 +2,8 @@ const accessToken = process.env.INSTAGRAM_TOKEN;
 
 export const getPosts = () =>
   fetch(
-    `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type&access_token=${accessToken}`
+    `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type&access_token=${accessToken}`,
+    { next: { revalidate: 3600 } }
   )
     .then((response) => response.json())
     .then((response) => {
