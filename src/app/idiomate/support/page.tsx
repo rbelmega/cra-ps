df9a6591fdb6362d79db5c9322bb2508f5e58fed
Rostyslav Belmeha <belmega31@gmail.com>
2026-03-31 00:15:48 +0300

    support

--- src/app/idiomate/support/page.tsx
+++ src/app/idiomate/support/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import { loadPublicText } from '../../domain/public-content';
+import { loadPublicText } from '../../../domain/public-content';
 
 export default async function Page() {
   const data = await loadPublicText('support.md');
