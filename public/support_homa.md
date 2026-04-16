0d08e2ec8b8e112142fa9f53cfc78fcf19662b2d
Rostyslav Belmeha <belmega31@gmail.com>
2026-04-16 16:23:30 +0300

    homa support

---
+++ public/support_homa.md
@@ -0,0 +1,3 @@
+### Homa Support
+If you have questions, feedback, or found an issue, contact us:
+belmega31@gmail.com
---
+++ src/app/homa/support/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { loadPublicText } from '../../../domain/public-content';
+
+export default async function Page() {
+  const data = await loadPublicText('support_homa.md');
+
+  return (
+    <section className="privacy-policy">
+      <ReactMarkdown children={data} />
+    </section>
+  );
+}
